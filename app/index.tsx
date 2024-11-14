59f7d9f8e6c8a8a32324b695257394bfae6d2dde
ziyiliu02 <132341835+ziyiliu02@users.noreply.github.com>
2024-11-14 16:12:07 +0800

    update welcome page

--- app/index.tsx
+++ app/index.tsx
@@ -1,16 +1,39 @@
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { StyleSheet, Text, View, TouchableOpacity, Image } from "react-native";
 import React from "react";
+import { StatusBar } from 'expo-status-bar';
 import { useRouter } from "expo-router";
+import Colors from "@/constants/Colors";
+import { useFonts } from 'expo-font';
 
 const Page = () => {
   const router = useRouter();
 
+  const [fontsLoaded] = useFonts({
+    'PermanentMarker': require('@/assets/fonts/PermanentMarker-Regular.ttf'),
+  });
+
+  if(!fontsLoaded) {
+    return null;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Welcome Page</Text>
-      <TouchableOpacity onPress={() => router.replace("/(tabs)")}>
-        <Text>Go to Home Screen</Text>
-      </TouchableOpacity>
+      <StatusBar style="light"/>
+      <View style={styles.logoWrapper}>
+        <Image source={require('@/assets/images/logo.png')} style={styles.logoImage}/>
+        <Text style={styles.logoText}>Cherrypick</Text>
+      </View>
+      <View style={styles.authWrapper}>
+        <TouchableOpacity onPress={() => router.push("/auth/register")} style={styles.registerBtn}>
+          <Text style={styles.registerBtnTxt}>Join :)</Text>
+        </TouchableOpacity>
+        <Text style={styles.loginWrapper}>
+          Already have an account?
+          <TouchableOpacity onPress={() => router.replace("/(tabs)")} style={{ marginTop: -2}}> 
+            <Text style={styles.loginTxt}>Login</Text>
+          </TouchableOpacity>
+        </Text>
+      </View>
     </View>
   );
 };
@@ -20,7 +43,49 @@ export default Page;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+    backgroundColor: Colors.primaryColorYellow
   },
+  logoWrapper: {
+    flex: 4, 
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  logoImage: {
+    width: 150,
+    height: 150
+  },
+  logoText: {
+    fontFamily: 'PermanentMarker',
+    fontSize: 32,
+    color: 'white'
+  },
+  authWrapper: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    paddingBottom: 50
+  },
+  registerBtn: {
+    backgroundColor: 'white',
+    paddingVertical: 12,
+    paddingHorizontal: 24,
+    borderRadius: 10,
+    marginBottom: 10,
+    marginHorizontal: 20
+  },
+  registerBtnTxt: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: Colors.primaryColorYellow,
+    textAlign: 'center'
+  },
+  loginWrapper: {
+    color: 'white',
+    textAlign: 'center',
+    alignItems: 'center'
+  }, 
+  loginTxt: {
+    color:'white',
+    fontWeight: '800',
+    marginLeft: 5
+  }
 });
